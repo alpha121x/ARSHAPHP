2f18fbc9623b5ab8c8c6fd3315e8cf8d5be662c3
Abbas Ch <109143082+alpha121x@users.noreply.github.com>
2023-10-28 03:24:24 +0500

    index.php components completed

---
+++ Arsha/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title>Arsha Bootstrap Template - Index</title>
+
+  <?php include("includes/linked-files.php") ?>
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php include("includes/header-nav.php") ?>
+  <!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <?php include("includes/hero-section.php") ?>
+  <!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <?php include("includes/main/clients-section.php") ?>
+    <!-- End Cliens Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <?php include("includes/main/about-us-section.php") ?>
+    <!-- End About Us Section -->
+
+    <!-- ======= Why Us Section ======= -->
+    <?php include("includes/main/why-us-section.php") ?>
+    <!-- End Why Us Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <?php include("includes/main/skills-section.php") ?>
+    <!-- End Skills Section -->
+
+    <!-- ======= Services Section ======= -->
+    <?php include("includes/main/services-section.php")?>
+    <!-- End Services Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <?php include("includes/main/cta-section.php") ?>
+    <!-- End Cta Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <?php include("includes/main/portfolio-section.php") ?>
+    <!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+    <?php include("includes/main/team-section.php") ?>
+    <!-- End Team Section -->
+
+    <!-- ======= Pricing Section ======= -->
+    <?php include("includes/main/pricing-section.php") ?>
+    <!-- End Pricing Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <?php include("includes/main/faq-section.php") ?>
+    <!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <?php include("includes/main/contact-section.php") ?>
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include("includes/footer.php") ?>
+  <!-- End Footer -->
+
+  <!-- ======= Footer ======= -->
+  <?php include("includes/preloader.php") ?>
+   <!-- ======= End Footer  ======= -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+  <!-- ======= js files ======= -->
+  <?php include("includes/linked-js-src-files.php") ?>
+   <!-- ======= End js files ======= -->
+
+</body>
+
+</html>
