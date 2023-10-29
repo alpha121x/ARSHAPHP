8676f0d3c866b6a66b96824376d3008978933371
Abbas Ch <109143082+alpha121x@users.noreply.github.com>
2023-10-29 20:33:28 +0500

    changes in Nice Admin

--- NiceAdmin/include/side-nav.php
+++ NiceAdmin/include/side-nav.php
@@ -8,34 +8,7 @@
     <i class="bi bi-journal-text"></i>
       <span>Dashboard</span>
     </a>
-  </li><!-- End Dashboard Nav -->
-
-  <li class="nav-item">
-        <a href="add-student.php" class="nav-link collapsed" >
-        <i class="bi bi-person-plus-fill"></i>
-          <span>Add Student</span>
-        </a>
-      </li><!-- End Forms Nav -->
-
-      <li class="nav-item">
-        <a class="nav-link collapsed"  href="view-students.php">
-        <i class="bi bi-eye"></i>
-          <span>View Student</span>
-        </a>
-      </li><!-- End DataTable Nav -->
-      
-      <li class="nav-item">
-        <a class="nav-link collapsed"  href="edit-student.php">
-        <i class="bi bi-pencil-square"></i>
-          <span>Edit Student</span>
-        </a>
-      </li><!-- End DataTable Nav -->  
-      <li class="nav-item">
-        <a class="nav-link collapsed"  href="view-students-info.php">
-        <i class="bi bi-journal-text"></i>
-          <span>View Student Info</span>
-        </a>
-      </li><!-- End DataTable Nav --> 
+  </li><!-- End Dashboard Nav --> 
       <li class="nav-item">
         <a class="nav-link collapsed"  href="add-admin-profile.php">
         <i class="bi bi-person-fill-add"></i>
