6be84711d2b112c3af0992eff40c0b5aa9ec2530
Abbas Ch <109143082+alpha121x@users.noreply.github.com>
2023-10-29 14:49:14 +0500

    upadtes

--- includes/inner-page/header-nav.php
+++ includes/inner-page/header-nav.php
@@ -2,8 +2,6 @@
     <div class="container d-flex align-items-center">
 
       <h1 class="logo me-auto"><a href="index.html">Arsha</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
